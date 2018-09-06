34ad07959648af107d63e6af316c7e02345954e4
Vaassous <43050890+Vaassous@users.noreply.github.com>
2018-09-07 01:17:40 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const cfg = require('./index.json');
+const prefix = ("Todd");
+
+bot.on('ready', function () {
+    console.log("Je suis prêt à être utilisé.")
+    bot.user.setActivity('Fume des splifs avec Bojack').catch(console.error)
+});
+
+bot.on('guildMemberAdd', member => {
+    member.createDM().then(channel => {
+        return channel.send('Bienvenue sur le serveur soup' + member.displayName)
+        console.log(`${member.displayName} à rejoind le serveur.`)
+    }).catch(console.error)
+});
+
+
+bot.on('message', msg => {
+    if (msg.content === "bonjour"){
+        msg.reply("Hey Salut, ça va ?")
+    }
+	if (msg.content.match(/nazi/i)) {
+            msg.reply('Woaw c\'est pas le lieu pour parler de nazisme.')
+    }
+    if (msg.content === prefix + "youtube"){
+        msg.channel.send("https://www.youtube.com/channel/UC3jCKKdmmyAE8pKU9OOCLyg?view_as=subscriber")
+        console.log("Une personne a demandé pour aller sur youtube.")
+    }
+
+});
+
+bot.login(cfg.token);
