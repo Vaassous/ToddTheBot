73b5b09c2dd9685aaf2bdadee42d087a41edb43e
Vaassous <43050890+Vaassous@users.noreply.github.com>
2018-09-07 01:59:06 +0200

    Update index.js

--- index.js
+++ index.js
@@ -21,9 +21,6 @@ bot.on('message', msg => {
     if (msg.content === "bonjour"){
         msg.reply("Hey Salut, ça va ?")
     }
-	if (msg.content.match(/nazi/i)) {
-            msg.reply('Woaw c\'est pas le lieu pour parler de nazisme.')
-    }
     if (msg.content === prefix + "youtube"){
         msg.channel.send("https://www.youtube.com/channel/UC3jCKKdmmyAE8pKU9OOCLyg?view_as=subscriber")
         console.log("Une personne a demandé pour aller sur youtube.")
